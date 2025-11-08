017468dfbc8ee84a23e4ad6fad28100e75dc5ab1
Tanay <t2jagade@uwaterloo.ca>
2025-11-08 12:18:02 -0500

    added audios and using error handling

--- src/functions/discordbot.py
+++ src/functions/discordbot.py
@@ -1,5 +1,6 @@
 import requests
 from playsound import playsound
+import random
 
 def post_to_discord(event_type, confidence=None, image_path=None):
     """
@@ -10,11 +11,16 @@ def post_to_discord(event_type, confidence=None, image_path=None):
     """
     WEBHOOK_URL = "https://discord.com/api/webhooks/1436744629197213817/_8njr0_0AjymTAvXMzup7pUF0eKFVxR-jJJgG2oN9XDKvLmif29RJ-XADMYr_Xc4k8_8"
     
-    # Play funny sound effect when detection happens
+    #List of sounds to loop through
+
+    sounds = ["sounds/alert.mp3", "sounds/alert1.mp3", "sounds/alert2.mp3", "sounds/alert3.mp3"]
+
+# Play funny sound effect when detection happens
     try:
-        playsound("/Users/tanayj/GoonHacks/src/functions/alert.mp3")
-    except:
-        print("⚠️ Could not play sound file")
+        random_sound = random.choice(sounds)
+        playsound(random_sound)
+    except Exception as e:
+        print(f"⚠️ Could not play sound file: {e}")
     
     # Create message - replace underscores with spaces and capitalize
     message = f"👀 Detected **{event_type.replace('_', ' ').title()}**"
